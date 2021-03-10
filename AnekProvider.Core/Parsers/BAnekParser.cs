07179fd1bbdc9405344845cbc64a5e38d9dc10c1
Vitali Dzikavitski <vitaldikov@gmail.com>
2021-03-10 19:16:26 +0300

    cerate base parser

---
+++ AnekProvider.Core/Parsers/BAnekParser.cs
@@ -0,0 +1,32 @@
+﻿using AnekProvider.DataModels.Entities;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Linq;
+
+namespace AnekProvider.Core.Parsers
+{
+    public class BAnekParser : IParser<BAnek>
+    {
+        public BAnek GetAnek(string url)
+        {
+            string htmlCode;
+            using (WebClient client = new WebClient())
+            {
+                htmlCode = client.DownloadString(url);
+            }
+
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(htmlCode);
+            string ClassToGet = "block-content mdl-card__supporting-text mdl-color--grey-300 mdl-color-text--grey-900";
+            var trNode = html.DocumentNode.SelectSingleNode($"//div[contains(@class, '{ClassToGet}')]");
+
+            foreach (HtmlNode node in trNode.SelectNodes("//br"))
+                node.ParentNode.ReplaceChild(html.CreateTextNode("\n"), node);
+
+            return new BAnek() { Text = trNode.InnerText, Uri = new Uri(url)};
+        }
+    }
+}
---
+++ AnekProvider.Core/Parsers/IParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnekProvider.Core.Parsers
+{
+    public interface IParser<TAnek> where TAnek: class
+    {
+        public TAnek GetAnek(string url);
+    }
+}
