7f892505460a321ccd568ae6701a8dda1698550d
Vitali Dzikavitski <vitaldikov@gmail.com>
2021-03-10 19:15:53 +0300

    create base anek

---
+++ AnekProvider.DataModels/Entities/BAnek.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnekProvider.DataModels.Entities
+{
+    public class BAnek
+    {
+        public Uri Uri { get; set; } 
+        public string Text { get; set; }
+    }
+}
