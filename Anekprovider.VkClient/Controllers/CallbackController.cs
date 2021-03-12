cc4e23903cba7e24a0cd67f533daa03dbb011df3
Vitali Dzikavitski <vitaldikov@gmail.com>
2021-03-12 12:19:48 +0300

    init VkClient

---
+++ Anekprovider.VkClient/Controllers/CallbackController.cs
@@ -0,0 +1,61 @@
+﻿using Anekprovider.VkClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+
+namespace Anekprovider.VkClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IVkApi _vkApi;
+
+        /// <summary>
+        /// Конфигурация приложения
+        /// </summary>
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
+        {
+            _vkApi = vkApi;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Callback([FromBody] UpdatesDto updates)
+        {
+            // Проверяем, что находится в поле "type" 
+            switch (updates.Type)
+            {
+                // Если это уведомление для подтверждения адреса
+                case "confirmation":
+                    // Отправляем строку для подтверждения 
+                    return Ok(_configuration["Config:Confirmation"]);
+
+                case "message_new":
+                    {
+                        // Десериализация
+                        var msg = Message.FromJson(new VkResponse(updates.Object));
+
+                        // Отправим в ответ полученный от пользователя текст
+                        _vkApi.Messages.Send(new MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            PeerId = msg.PeerId.Value,
+                            Message = msg.Text
+                        });
+                        break;
+                    }
+            }
+            // Возвращаем "ok" серверу Callback API
+            return Ok("ok");
+        }
+    }
+}
---
+++ Anekprovider.VkClient/Models/UpdatesDto.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anekprovider.VkClient.Models
+{
+    [Serializable]
+    public class UpdatesDto
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("object")]
+        public JObject Object { get; set; }
+
+        [JsonProperty("group_id")]
+        public long GroupId { get; set; }
+
+        [JsonProperty("event_id")]
+        public string EventId { get; set; }
+    }
+}
